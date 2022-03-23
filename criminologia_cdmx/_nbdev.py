22988f65aef954df1df73f9a6a6951f518c684df
pablo <pablo.lopez@centrogeo.edu.mx>
2022-03-23 18:51:51 +0000

    Corrección a commits anteriores.

--- criminologia_cdmx/_nbdev.py
+++ criminologia_cdmx/_nbdev.py
@@ -25,7 +25,8 @@ index = {"DATA_PATH": "02_covariables.ipynb",
          "serie_razones_de_eventos": "01_patrones_espacio_temporales.ipynb",
          "serie_mapas_intensidad": "01_patrones_espacio_temporales.ipynb",
          "p_value_maps": "01_patrones_espacio_temporales.ipynb",
-         "descarga_datos_covariables": "02_covariables.ipynb"}
+         "descarga_datos_covariables": "02_covariables.ipynb",
+         "get_diccionario_censo": "02_covariables.ipynb"}
 
 modules = ["etl.py",
            "patrones_espacio_temporales.py",
