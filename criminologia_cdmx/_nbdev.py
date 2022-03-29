64da46965c8d16048c303e141cdeb0a838af5391
pablo <pablo.lopez@centrogeo.edu.mx>
2022-03-29 19:38:03 +0000

    Clase ModeloGLM.

--- criminologia_cdmx/_nbdev.py
+++ criminologia_cdmx/_nbdev.py
@@ -35,7 +35,8 @@ index = {"DATA_PATH": "02_covariables.ipynb",
          "agrega_uso_suelo": "02_covariables.ipynb",
          "IndicePCA": "02_covariables.ipynb",
          "variable_independiente": "03_modelos.ipynb",
-         "CapaDeAnalisis": "03_modelos.ipynb"}
+         "CapaDeAnalisis": "03_modelos.ipynb",
+         "ModeloGLM": "03_modelos.ipynb"}
 
 modules = ["etl.py",
            "patrones_espacio_temporales.py",
