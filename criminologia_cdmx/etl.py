64d9f1f8fcfc600f3276ef7fe3809f4dd01b62c9
pablo <pablo.lopez@centrogeo.edu.mx>
2022-05-04 00:20:23 +0000

    Nuva forma de asignar ids espaciales
    Incluye asignación a manzanas

--- criminologia_cdmx/etl.py
+++ criminologia_cdmx/etl.py
@@ -2,7 +2,7 @@
 
 __all__ = ['DATA_PATH', 'DOWNLOADS_PATH', 'procesa_registros', 'get_carpetas_from_api', 'get_victimas_from_api',
            'get_historico_carpetas', 'get_historico_victimas', 'get_carpetas_desde_archivo',
-           'get_victimas_desde_archivo', 'agrega_ids_espaciales', 'agregar_categorias_carpetas',
+           'get_victimas_desde_archivo', 'descarga_manzanas', 'agrega_ids_espaciales', 'agregar_categorias_carpetas',
            'agregar_categorias_victimas', 'exporta_datos_visualizador', 'serie_de_tiempo_categoria',
            'serie_tiempo_categorias_unidades', 'punto_to_hexid', 'agrega_en_hexagonos']
 
@@ -105,23 +105,57 @@ def get_victimas_desde_archivo(archivo):
     return records
 
 # Cell
-def agrega_ids_espaciales(carpetas):
-    """Agrega ids de colonias y cuadrantes a la base de carpetas."""
+def descarga_manzanas():
+    """ Descarga la geometría de manzanas con ids de cuadrante y colonia."""
+    if os.path.exists(DOWNLOADS_PATH + 'manzanas_identificadores.gpkg'):
+        print("El archivo ya está descargado.")
+    else:
+        url = "https://www.dropbox.com/s/a370kmtknhgca2y/manzanas_identificadores.gpkg?dl=1"
+        r = requests.get(url, allow_redirects=True)
+        open(DOWNLOADS_PATH + 'manzanas_identificadores.gpkg', 'wb').write(r.content)
+
+# Cell
+def agrega_ids_espaciales(carpetas, metodo='manzanas', tolerancia=500):
+    """ Agrega ids de colonias y cuadrantes a la base de carpetas.
+
+        Args:
+
+            metodo (str): manzanas/poligonos. El método 'manzanas' hace un join_nearest con
+                          las manzanas que ya tienen ids de cuadrante y polígono; el método poligonos
+                          hace la unión espacial de los incidentes con las geometrías.
+            tolerancia (float): ¿qué tan lejos puede estar un incidente de una manzana? En el
+                                método 'poligonos' se ignora
+
+    """
+
+    if 'manzana_cvegeo' in carpetas.columns:
+        carpetas = carpetas.drop(columns='colonia_cve')
     if 'colonia_cve' in carpetas.columns:
         carpetas = carpetas.drop(columns='colonia_cve')
     if 'cuadrante_id' in carpetas.columns:
         carpetas = carpetas.drop(columns='cuadrante_id')
     if 'municipio_cvegeo' in carpetas.columns:
         carpetas = carpetas.drop(columns='municipio_cvegeo')
-
-    shapes = os.path.join(DATA_PATH, 'criminologia_capas.gpkg')
-    colonias = gpd.read_file(shapes, layer='colonias').drop(columns='colonia_geom_6362')
-    cuadrantes = gpd.read_file(shapes, layer='cuadrantes')
-    carpetas = (gpd.tools.sjoin(carpetas, colonias[['colonia_cve', 'colonia_nombre', 'municipio_cvegeo', 'geometry']])
-                .drop(columns=['index_right'])
-               )
-    carpetas = (gpd.tools.sjoin(carpetas, cuadrantes[['cuadrante_id', 'geometry']])
-                .drop(columns=['index_right']))
+    if metodo == 'poligonos':
+        shapes = os.path.join(DATA_PATH, 'criminologia_capas.gpkg')
+        colonias = gpd.read_file(shapes, layer='colonias').drop(columns='colonia_geom_6362')
+        cuadrantes = gpd.read_file(shapes, layer='cuadrantes')
+        carpetas = (gpd.tools.sjoin(carpetas, colonias[['colonia_cve', 'colonia_nombre', 'municipio_cvegeo', 'geometry']])
+                    .drop(columns=['index_right'])
+                   )
+        carpetas = (gpd.tools.sjoin(carpetas, cuadrantes[['cuadrante_id', 'geometry']])
+                    .drop(columns=['index_right']))
+    elif metodo == 'manzanas':
+        crs_original = carpetas.crs
+        manzanas = gpd.read_file("datos/descargas/manzanas_identificadores.gpkg")
+        manzanas['municipio_cvegeo'] = manzanas['CVE_ENT'] + manzanas['CVE_MUN']
+        carpetas = (carpetas
+                    .to_crs(manzanas.crs)
+                    .sjoin_nearest(manzanas[['CVEGEO', 'municipio_cvegeo', 'colonia_cve',
+                                             'cuadrante_id', 'geometry']], max_distance=tolerancia)
+                    .rename({'CVEGEO': 'manzana_cvegeo'}, axis=1)
+                    .drop(columns='index_right')
+                    .to_crs(crs_original))
     return carpetas
 
 # Cell
